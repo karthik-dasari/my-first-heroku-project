488d0cfb2e065de146fbdb1d71064498f3c98d3d
Karthik Dasari <67855672+karthik-dasari@users.noreply.github.com>
2021-04-09 22:48:10 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client 
  
-account_sid = 'AC48a591cb952123ed25f2256c870e1c9b' 
-auth_token = 'c2d4be1f8b14fea8d10c2c356f875246' 
+account_sid = '' 
+auth_token = '' 
 client = Client(account_sid, auth_token) 
 def send_message():
 
     message = client.messages.create( 
-                              from_='whatsapp:+14155238886',  
+                              from_='whatsapp:',  
                               body='hi',      
-                              to='whatsapp:+917702329668' 
+                              to='whatsapp:' 
                           ) 
  
     print(message.sid)
